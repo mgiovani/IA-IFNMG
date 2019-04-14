dfe90076b22150125bde643412a427f25ec0a9a4
Giovani Moutinho <giovanim@ymail.com>
2019-04-14 18:19:54 -0300

    Create mutate and update population

--- Trabalhos/01-Algoritmo-Genético/src/genetic_algorithm.py
+++ Trabalhos/01-Algoritmo-Genético/src/genetic_algorithm.py
@@ -10,6 +10,7 @@
 # 6. Repita 2 a 5 até atingir o número máximo de gerações. Sugestão: 100 gerações.
 # Depois do algoritmo pronto, execute-o 30 vezes. Para cada execução salve o melhor indivíduo ao final das gerações. Portanto, serão 30 indivíduos diferentes.
 
+import copy
 import math
 import numpy as np
 import random
@@ -29,28 +30,38 @@ def fitness_function(x):
     return abs(abs(np.min(x))-constants.X_MAX)
 
 def select_individuals(population):
-    best = population[get_best_individual_position(population)]
+    best_individual = copy.deepcopy(population[get_best_individual_position(population)])
     selected_individuals = start_tournament(population)
-    return selected_individuals, best
+    return selected_individuals, best_individual
 
 def crossover(parent_1, parent_2):
     # Não necessário para esta entrega
     pass
 
-def mutate(individual):
-    pass
+def mutate(population):
+    for individual in population:
+        if random.random() <= constants.MUTATION_RATE:
+            pos = random.randint(0, len(individual)-1)
+            noise = constants.NOISE_MAX if random.randint(0, 1) == 1 else constants.NOISE_MIN
+            individual[pos] += individual[pos] * noise
+    return population
 
-def update_for_next_generation():
-    pass
+def update_for_next_generation(population, best_individual):
+    population[get_worst_individual_position(population)] = copy.deepcopy(best_individual)
+    return population
 
-def population_has_converged():
-    pass
+
+def population_has_converged(population):
+    for individual in population:
+        if individual != population[0]:
+            return False
+    return True
 
 def start_tournament(population):
     selected_individuals = []
     for _ in range(len(population)):
-        individual_1 = population[random.randint(0, len(population)-1)]
-        individual_2 = population[random.randint(0, len(population)-1)]
+        individual_1 = copy.deepcopy(population[random.randint(0, len(population)-1)])
+        individual_2 = copy.deepcopy(population[random.randint(0, len(population)-1)])
         if fitness_function(individual_1) > fitness_function(individual_2):
             selected_individuals.append(individual_1)
         else:
@@ -66,12 +77,25 @@ def get_best_individual_position(population):
             best_ind = i
     return best_ind
 
+def get_worst_individual_position(population):
+    worst = fitness_function(population[0])
+    worst_ind = 0
+    for i in range(len(population)):
+        if fitness_function(population[i]) < worst:
+            worst = fitness_function(population[i])
+            worst_ind = i
+    return worst_ind
+
 def main():
-    ini_population = initial_population()
-    print('População Inicial: ', *ini_population, sep='\n')
+    population = initial_population()
+    print('População Inicial: ', *population, sep='\n')
     for generation in range(1, 101):
-        sel_individuals, best_individual = select_individuals(ini_population)
-        print('\nIndivíduos Selecionados: ', *sel_individuals, sep='\n')
+        population, best_individual = select_individuals(population)
+        print('\nIndivíduos Selecionados: ', *population, sep='\n')
+        population = mutate(population)
+        print('\nIndivíduos Após Mutação: ', *population, sep='\n')
+        population = update_for_next_generation(population, best_individual)
+        print('\nIndivíduos Após Atualização: ', *population, sep='\n')
         print('\nGeração Atual: ', generation)
         print('Melhor Indivíduo: ', best_individual)
 
